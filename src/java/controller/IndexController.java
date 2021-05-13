24c4e02a61d8481ae89f9ce3509ecd20f1e149ca
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2021-05-14 04:24:41 +1000

    Add dynamic index page

--- src/java/controller/IndexController.java
+++ src/java/controller/IndexController.java
@@ -1,7 +1,8 @@
 package controller;
 
 import layout.BaseController;
-import model.*;
+import model.CartService;
+import model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,35 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CartController", urlPatterns = {"/cart"})
-public class CartController extends BaseController {
+@WebServlet(name = "IndexController", urlPatterns = {"/"})
+public class IndexController extends BaseController {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!ensureUserLoggedIn(request, response)) {
-            return;
-        }
-
-        CartService service = new CartService();
-
-        request.setAttribute("cart", service.cart(getUserSession(request)));
-        view("cart", "Cart", request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!ensureUserLoggedIn(request, response)) {
-            return;
-        }
-
-        int id = Integer.parseInt(request.getParameter("id"));
-        User user = getUserSession(request);
-
-        CartService service = new CartService();
-        if (!service.addProduct(user, id)) {
-            response.sendRedirect("products?error=Product+Already+In+Cart");
-            return;
-        }
-
-        response.sendRedirect("cart");
+        view("index", "Python Discussion Forum", request, response);
     }
 }
--- web/views/index.jsp
+++ web/views/index.jsp
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Python Discussion Forum</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <p><strong>Project By: </strong> Tom Grozev</p>
+<h1>Welcome to the Python Discussion Forum.</h1>
 
-    <h1>Welcome to the Python Discussion Forum.</h1>
-
-    <a href="topics">Check out the forums</a>
-    <br>
-    <a href="products">Check out the new products for sale</a>
-</body>
-</html>
+<a href="topics">Check out the forums</a>
+<br>
+<a href="products">Check out the new products for sale</a>
